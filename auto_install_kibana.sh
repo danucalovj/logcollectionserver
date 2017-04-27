f16a69d81dfcef4b5451d607cc0406953ada1db8
danucalovj <danucalovj@users.noreply.github.com>
2017-04-27 10:55:46 -0700

    Update auto_install_kibana.sh

--- auto_install_kibana.sh
+++ auto_install_kibana.sh
@@ -10,13 +10,13 @@ wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add 
 sudo apt-get -y install apt-transport-https
 
 # Updates Repository Sources
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+# echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 
-#echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
+echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
 sudo add-apt-repository -y ppa:webupd8team/java
-#echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
-#echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash-2.2.x.list
-#echo "deb https://packages.elastic.co/beats/apt stable main" |  sudo tee -a /etc/apt/sources.list.d/beats.list
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+# echo 'deb http://packages.elastic.co/logstash/2.2/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash-2.2.x.list
+echo "deb https://packages.elastic.co/beats/apt stable main" |  sudo tee -a /etc/apt/sources.list.d/beats.list
 
 # Apt-Get Update Everything
 sudo apt-get update
@@ -58,6 +58,8 @@ sudo service elasticsearch restart
 sudo update-rc.d elasticsearch defaults 95 10
 
 # Install Logstash
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo apt-get update
 sudo apt-get -y install logstash
 sudo mkdir -p /etc/pki/tls/certs
 sudo mkdir /etc/pki/tls/private
